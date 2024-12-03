c86782d37d848eb5284cf5254ab1e67863cceeda
SHIMOSAKA Kota <cguf0060@mail4.doshisha.ac.jp>
2024-12-03 12:01:59 +0900

    fix: 表示修正, webhook投稿

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
-const BAR_LENGTH = 20;
+const BAR_LENGTH = 20; // スマホの表示的にこれぐらいがちょうどいい感じ
+const WEBHOOK_URL = "URL";
 
-// 年初の日付を取得
 const getYearStart = (year) => new Date(year, 0, 1);
-
-// 年末の日付を取得
 const getYearEnd = (year) => new Date(year, 11, 31);
 
-// 干支の絵文字リスト（申年から始まる順）
+// mod 12の剰余で0:申~で求められる
 const etoEmojis = ["🐵", "🐔", "🐶", "🐷", "🐭", "🐮", "🐯", "🐰", "🐲", "🐍", "🐴", "🐑"];
 
-// 干支の絵文字を取得する関数
 const getEtoEmoji = (year) => etoEmojis[year % 12];
 
-// 進捗バーの位置を計算する関数
 const calculateEtoPosition = (progress) => {
     const position = Math.floor(progress * (BAR_LENGTH - 1));
     return Math.max(0, Math.min(position, BAR_LENGTH - 1));
 };
 
+// webhook周りはちゃんと非同期でやったほうがいいらしい
+const postWebhook = async (url, data) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+        throw new Error(`Failed to post webhook: ${response.status}`);
+    }
+}
+
 const today = new Date();
 const currentYear = today.getFullYear();
 
-// 昨日の日付を取得
 const yesterday = new Date(today);
 yesterday.setDate(today.getDate() - 1);
 
@@ -30,18 +40,31 @@ const yearEnd = getYearEnd(currentYear);
 
 const totalYearMilliseconds = yearEnd - yearStart;
 const todayProgress = (today - yearStart) / totalYearMilliseconds;
-const todayProgressPercent = Math.floor(todayProgress * 100); // 百分率に変換
+const todayProgressPercent = Math.floor(todayProgress * 100);
 const yesterdayProgress = (yesterday - yearStart) / totalYearMilliseconds;
-const yesterdayProgressPercent = Math.floor(yesterdayProgress * 100); // 百分率に変換
+const yesterdayProgressPercent = Math.floor(yesterdayProgress * 100);
 
-// 進捗率が昨日から変わった場合にのみ表示
+// 進捗率が昨日から変わった場合にのみ表示(n%が続くと嫌なので)
 if (todayProgressPercent !== yesterdayProgressPercent) {
     const progressArray = Array(BAR_LENGTH).fill("＿");
     const etoEmoji = getEtoEmoji(currentYear);
     const etoPosition = calculateEtoPosition(todayProgress);
-    progressArray[etoPosition] = etoEmoji;
+    
+    if (etoPosition == 0 || etoPosition == BAR_LENGTH - 1) {
+        progressArray[etoPosition] = etoEmoji;
+    } else {
+        progressArray[etoPosition - 1] = "…";
+        progressArray[etoPosition] = etoEmoji;
+    }
 
     const progressBar = `←${currentYear - 1}${progressArray.join('')}${currentYear + 1}→`;
 
-    console.log(`今日で${currentYear}年の${todayProgressPercent}%が経過しました。\n${progressBar}`);
+    const message = `${currentYear} is ${todayProgressPercent}% complete. \n${progressBar}`;
+    console.log(message);
+    (async () => {
+        await postWebhook(WEBHOOK_URL, {
+            content: message,
+        });
+    }
+    )();
 }
